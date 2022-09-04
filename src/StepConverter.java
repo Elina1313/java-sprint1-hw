2185b8a4b3187870bee0cd2806abe0a211b86e4f
Elinka133 <lino4ka13@yandex.ru>
2022-09-05 02:48:22 +0300

    ТЗ 1 спринта. Запись в файлы.

---
+++ src/StepConverter.java
@@ -0,0 +1,11 @@
+public class StepConverter {
+    double converterKm(int sumMonth) {
+        double km = 0.00075;
+        return sumMonth * km;
+    }
+    public double converterKcal(int sumMonth) {
+        double kcal = 0.05;
+        return sumMonth * kcal;
+
+    }
+}
